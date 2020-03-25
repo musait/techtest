7d9ea4c9fb525a1e10b68552acd6785e3154c12f
Mustapha <ait.mus06@gmail.com>
2020-03-25 17:17:58 +0100

    modify route to access items through deliveries

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :delivery_items
-  resources :deliveries
+  resources :deliveries do
+    resources :items, only: [:index, :show]
+  end
   resources :items
   match '*any' => 'application#options', :via => [:options]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

6bb0558b4eff76d74ad15234a0393948652014fa
Mustapha <ait.mus06@gmail.com>
2020-03-25 17:16:30 +0100

    add configuration to permit api call from localhost:3000

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :delivery_items
   resources :deliveries
   resources :items
+  match '*any' => 'application#options', :via => [:options]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

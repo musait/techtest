ccdd2834d4cbfd69f7d79475f0d9c645efb92b52
Mustapha <ait.mus06@gmail.com>
2020-03-25 16:58:11 +0100

    add delivery table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_155609) do
+ActiveRecord::Schema.define(version: 2020_03_25_155710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deliveries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"

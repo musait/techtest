2c1e3b710486a0e9554a0e6d05793f1cb89ab9f2
Mustapha <ait.mus06@gmail.com>
2020-03-26 18:08:09 +0100

    add method to count and update quantity_ref field

--- app/models/delivery_item.rb
+++ app/models/delivery_item.rb
@@ -1,4 +1,11 @@
 class DeliveryItem < ApplicationRecord
   belongs_to :item
   belongs_to :delivery
+  after_create :increment_items_count
+
+  def increment_items_count
+    @delivery = Delivery.find(self.delivery_id)
+    @delivery.quantity_ref += 1
+    @delivery.save
+  end
 end

72c4648103d3d065b0a43f1a6ed54100e0aac167
Mustapha <ait.mus06@gmail.com>
2020-03-26 18:49:47 +0100

    add method to decrement items count when a relation is deleted

--- app/models/delivery_item.rb
+++ app/models/delivery_item.rb
@@ -2,10 +2,17 @@ class DeliveryItem < ApplicationRecord
   belongs_to :item
   belongs_to :delivery
   after_create :increment_items_count
+  after_destroy :decrement_items_count
 
   def increment_items_count
     @delivery = Delivery.find(self.delivery_id)
     @delivery.quantity_ref += 1
+    @delivery.save
+  end
+
+  def decrement_items_count
+    @delivery = Delivery.find(self.delivery_id)
+    @delivery.quantity_ref -= 1
     @delivery.save
   end
 end

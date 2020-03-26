5a443c9a8c88ffe8d86690a025829f3023543f5c
Mustapha <ait.mus06@gmail.com>
2020-03-26 18:08:40 +0100

    add route for delivery item finding method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :delivery_items
-  resources :deliveries do
-    resources :items, only: [:index, :show]
-  end
+
+  resources :deliveries
+  get '/deliveries/:id/items', to: 'deliveries#delivery_item'
   resources :items
+  resources :order_picker,only: [:index] do
+    resources :deliveries, only: [:show]
+  end
   match '*any' => 'application#options', :via => [:options]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
